5991736750a267d568e6d743b73a5e87cd056a9c
KevenMc <djeloniticsPASSWORD123>
2023-02-28 20:22:57 +0000

    eclat

--- association/eclat.py
+++ association/eclat.py
@@ -1,4 +1,4 @@
-"""Apriori association learning model"""
+"""Eclat association learning model"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -18,18 +18,20 @@ transactions = dataset.values.tolist()
 rules = apriori(transactions=transactions, min_support=0.003,
                 min_confidence=0.2, min_lift=3, min_length=2, max_length=2)
 
-#Visualise results
+# Visualise results
 results = list(rules)
+
 
 def inspect(results: List):
     """Convert results from apriori into a well formatted list"""
     lhs = [tuple(result[2][0][0]) for result in results]
     rhs = [tuple(result[2][0][1])[0] for result in results]
     supports = [result[1] for result in results]
-    confidence = [result[2][0][2] for result in results]
-    lifts = [result[2][0][3] for result in results]
-    return list(zip(lhs, rhs, supports, confidence, lifts))
+    return list(zip(lhs, rhs, supports))
 
-resultsDF = pd.DataFrame(inspect(results), columns=["Left hand side", "Right hand side", "Support", "Confidence", "Lift"])
-resultsDF = resultsDF.sort_values(by="Lift", ascending=False, ignore_index=True)
+
+resultsDF = pd.DataFrame(inspect(results), columns=[
+                         "Product 1", "Product 2", "Support"])
+resultsDF = resultsDF.sort_values(
+    by="Support", ascending=False, ignore_index=True)
 print(resultsDF[:20])
